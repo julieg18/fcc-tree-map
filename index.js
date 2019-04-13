3e4018f66dd4d9a7114a6f68a5229b5c66abebfb
julieg18 <julieheartcrochet@gmail.com>
2019-04-13 15:08:17 -0500

    added legend

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 let JSONlink = 'https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/movie-data.json'
-// 910 - 2950
-// 500
-let width = 1000
-let height = 700
+
+let treeMapWidth = 1000
+let treeMapHeight = 600
 let treeMapSvg = d3.select('#tree-map')
-  .attr('width', width)
-  .attr('height', height)
+  .attr('width', treeMapWidth)
+  .attr('height', treeMapHeight)
 let treemap = d3.treemap()
-  .size([width, height])
+  .size([treeMapWidth, treeMapHeight])
   .paddingInner(1)
 let colors = {
   'Action': 'red',
@@ -19,15 +18,25 @@ let colors = {
   'Biography': 'gray'
 }
 
+function makeGenreArray (obj) {
+  let arr = []
+  for (let prop in obj) {
+    arr.push(prop)
+  }
+  return arr
+}
+
 let req = new XMLHttpRequest()
 req.open('GET', JSONlink, true)
 req.send()
 req.onload = function () {
   let JSONdata = JSON.parse(req.responseText)
+
   let root = d3.hierarchy(JSONdata)
     .sum((d) => d.value)
     .sort((a, b) => b.height - a.height || b.value - a.value)
   treemap(root)
+
   let tiles = treeMapSvg.selectAll('g')
     .data(root.leaves())
     .enter()
@@ -43,4 +52,49 @@ req.onload = function () {
     .attr('data-value', (d) => d.data.value)
     .attr('class', 'tile')
     .attr('fill', (d) => colors[d.data.category])
+    .on('mousemove', (d) => {
+      d3.select('#tooltip')
+        .style('visibility', 'visible')
+        .style('left', `${event.pageX + 10}px`)
+        .style('top', `${event.pageY - 20}px`)
+        .attr('data-value', d.data.value)
+        .html(`
+          Name: ${d.data.name}
+          </br>
+          Category: ${d.data.category}
+          </br>
+          Value: ${d.data.value}
+        `)
+    })
+    .on('mouseout', (d) => {
+      d3.select('#tooltip')
+        .style('visibility', 'hidden')
+    })
+
+  let legendHeight = 20
+  let legendWidth = 800
+  let legendSvg = d3.select('#legend')
+    .attr('height', legendHeight)
+    .attr('width', legendWidth)
+  let genreArray = makeGenreArray(colors)
+
+  legendSvg
+    .selectAll('rect')
+    .data(genreArray)
+    .enter()
+    .append('rect')
+    .attr('height', 20)
+    .attr('width', 20)
+    .attr('x', (d, i) => i * (legendWidth / 7))
+    .attr('y', 0)
+    .attr('fill', (d) => colors[d])
+
+  legendSvg
+    .selectAll('text')
+    .data(genreArray)
+    .enter()
+    .append('text')
+    .text((d) => d)
+    .attr('x', (d, i) => i * (legendWidth / 7) + 25)
+    .attr('y', legendHeight - 5)
 }
